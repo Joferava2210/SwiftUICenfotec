dba51363ccf6fd18bff9794524683af23796a853
joferava <jfelipe221095@gmail.com>
2021-02-26 19:25:28 -0600

    Initial Commit

---
+++ swui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swui
+//
+//  Created by Felipe Ramirez Vargas on 26/2/21.
+//  Copyright © 2021 com.framirez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
